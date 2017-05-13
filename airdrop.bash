ab0a5560a0d4ceb7fb355d3d11e332087f790b13
Gustavo Magaña <g.maganalopez@ugto.mx>
2017-05-13 00:10:49 -0500

    Add files via upload
    If you like being all time working from the Terminal (as I do), perhaps you've encountered yourself wanting to receive some files via  AirDrop but hate to do all the gui nonsense of clicking to enable Wi-Fi and Bluetooth and then opening ~/Downloads/ in the Finder.
    In less than 30 lines and only one argument with two possible values, this script does it simple and fast from the command line.
    If you find any way to improve it, please contact me.

---
+++ airdrop.bash
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+flg=$1
+
+if [ -z "$flg" ]; then
+	echo " "
+	echo "airdrop: prepare your mac to recieive via AirDrop"
+	echo " Useage:"
+	echo " $ airdrop 1 ( turns on wifi and bluetooth, opens ~/Downloads in Finder )"
+	echo " $ airdrop 0 ( turns off just bluetooth, because I'm guessing you're always online )"
+	echo " "
+elif [ "$flg" == 1 ]; then
+	networksetup -setairportpower en1 on
+	blueutil power 1
+	open /Users/gmaganna/Downloads
+	echo " "
+	echo "AirDrop Ready!"
+	echo " "
+elif [ "$flg" == 0 ]; then
+	blueutil power 0
+	echo " "
+	echo "AirDrop off (bluetooth off)"
+	echo " "
+else {
+	echo " "
+	echo "Error, invalid option"
+	echo " "
+}
+fi
+##################################
+# PLEASE DO NOT REMOVE THIS NOTICE
+# Script written by Gustavo Magaña
+# License: MIT License
+#
+# * This script needs the utility 'blueutil' to be installed
+#   a priori. It can be installed via homebrew:
+#   $ brew install blueutil
+# 
+# * Depending on each machine, the folowing line: 
+#	networksetup -setairportpower en1 on
+#   should be modified, changing the 'en1' to 'en0'
+#   or something like that. Run it separately to check it works.
+#   $ networksetup -setairportpower en1 on
+#
+# * Move this file into /usr/local/bin/ to execute it.
+#   Permissions and ownership might need changes (use chown and chmod).
+#
+#################################
